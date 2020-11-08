520b9e3d27efc6f82c850fdf8f274429e2878321
Ari <aaari94@gmail.com>
2020-11-08 16:28:33 +0100

    Succesfully implemented date-fns instead of custom functions

--- src/components/DownPaymentPlan.svelte
+++ src/components/DownPaymentPlan.svelte
@@ -1,85 +1,51 @@
 <script>
     import Table from "./Table.svelte";
 
+    //Handle date's for the POST request:
+    import format from "date-fns/format";
+    import addDays from "date-fns/addDays";
+    import addMonths from "date-fns/addMonths";
+    import addYears from "date-fns/addYears";
+
     let showTable = false;
     let loading = false;
-
     let downpaymentPlan = "";
     const API_URL =
         "https://visningsrom.stacc.com/dd_server_laaneberegning/rest/laaneberegning/v1/nedbetalingsplan";
 
-    //Props recieved form parent component (UserInput) before being passed to POST request
+    //Props recieved from parent component (UserInput)
     export let loanAmount;
     export let interestRate;
     export let years;
     export let loanFee;
 
-    //Increment a datestring's day, month or year by 1 and returns the result as a string.
-    const incrementDate = (dateString, whatToIncrement = "date") => {
-        //Split dateString
-        var splitDate = dateString.split("-");
-
-        //Construct date object
-        var dt = new Date(
-            parseInt(splitDate[0], 10), // year
-            parseInt(splitDate[1], 10 - 1), // month (starts with 0)
-            parseInt(splitDate[2], 10) // date
-        );
-
-        if (whatToIncrement === "date") {
-            dt.setDate(dt.getDate() + 1);
-        }
-        if (whatToIncrement == "month") {
-            dt.setMonth(dt.getMonth() + 1);
-        }
-        if (whatToIncrement == "year") {
-            dt.setFullYear(dt.getFullYear() + 1);
-        }
-
-        splitDate[0] = dt.getFullYear();
-
-        splitDate[1] = "" + dt.getMonth();
-        if (splitDate[1].length < 2) {
-            //Concatenate "0" if month is represented with  1 digit.
-            splitDate[1] = "0" + splitDate[1];
-        }
-
-        splitDate[2] = "" + dt.getDate();
-        if (splitDate[2].length < 2) {
-            //Concatenate "0" if date is 1 digit.
-            splitDate[2] = "0" + splitDate[2];
-        }
-
-        return splitDate.join("-");
-    };
-
-    //Get today's date in the form a string accepted by the API. addYears will increase the year by X.
-    const getTodaysDateString = (addYears = 0) => {
+    const getTodaysDateString = (
+        daysToAdd = 0,
+        monthsToAdd = 0,
+        yearsToAdd = 0
+    ) => {
         let today = new Date();
-        let date = today.getDate();
-        let month = today.getMonth() + 1;
-        let yyyy = today.getFullYear();
 
-        //Append "0" to date if single digit
-        if (date.toString().length == 1) {
-            date = "0" + date;
-        }
+        today = addYears(today, yearsToAdd);
+        today = addMonths(today, monthsToAdd);
+        today = addDays(today, daysToAdd);
 
-        return `${yyyy + addYears || yyyy}-${month}-${date}`;
+        return format(today, "yyyy-dd-mm");
     };
 
     async function postLoan() {
         if (!showTable) {
-            loading = true;
+            //Only request data if table is hidden.
+            loading = true; //Flag loading process
             const myHeaders = new Headers();
             myHeaders.append("Content-Type", "application/json");
             let raw = JSON.stringify({
                 laanebelop: loanAmount,
                 nominellRente: interestRate,
                 terminGebyr: loanFee,
-                utlopsDato: getTodaysDateString(years),
-                saldoDato: incrementDate(getTodaysDateString(), "month"),
-                datoForsteInnbetaling: getTodaysDateString(0, 1),
+                utlopsDato: getTodaysDateString(0, 0, years),
+                saldoDato: getTodaysDateString((14, 0, 0)), //Customer recieves payment after 2 weeks.
+                datoForsteInnbetaling: getTodaysDateString(0, 1, 0), //Payments start after 1 month.
                 ukjentVerdi: "TERMINBELOP",
             });
 
@@ -93,11 +59,10 @@
             fetch(API_URL, requestOptions)
                 .then((response) => response.text())
                 .then((result) => (downpaymentPlan = JSON.parse(result)))
-                .then(() => console.log(downpaymentPlan))
                 .then(() => ((showTable = true), (loading = false)))
                 .catch((error) => console.log("error", error));
         } else {
-            showTable = false;
+            showTable = false; //Reset the flag upon second click.
         }
     }
 </script>
